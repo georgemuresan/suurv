ead852957315b2e830861aa74e280d2b11c2c028
George Muresan <georgecatalinmuresan@gmail.com>
2018-10-10 20:25:01 +0100

    Updates

---
+++ precache-manifest.f29581de2d432be030f52cc1370a0769.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/suurv/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "29feb82e7614c1a61c32",
+    "url": "/suurv/static/js/runtime~main.29feb82e.js"
+  },
+  {
+    "revision": "b862ad4fa272ff45a15e",
+    "url": "/suurv/static/js/main.b862ad4f.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/suurv/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "b862ad4fa272ff45a15e",
+    "url": "/suurv/static/css/main.cb93de55.chunk.css"
+  },
+  {
+    "revision": "1b195a59a58f937af938d095bd695558",
+    "url": "/suurv/index.html"
+  }
+];
